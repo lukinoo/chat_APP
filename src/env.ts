32176c0fb904bb9e1f8d92751624c87b870ff17d
lukinoo <luka.bliadze.2015@mail.ru>
2022-06-26 18:30:33 +0400

    chore:changed-env-file

---
+++ src/env.ts
@@ -0,0 +1,10 @@
+export enum ConfigVariables {
+  API_KEY = "AIzaSyANR15IHNttzgyvIqm_WQ9yacOEiN4ac_E",
+  AUTH_DOMAIN = "realtime-chatapp-c6886.firebaseapp.com",
+  PROJECT_ID = "realtime-chatapp-c6886",
+  STORAGE_BUCKET = "realtime-chatapp-c6886.appspot.com",
+  MESSAGING_SENDERID = "713996240601",
+  APP_ID = "1:713996240601:web:cfac65e69353af4a61a0c8",
+}
+
+export const owner: string = "KOeK8FHCNFNp0EA7INKTEk5p1LU2";
